a538ef5d5609ec26a717e35d4e3b09bcb64cd084
nionis <stivennoni77@gmail.com>
2020-03-25 03:12:10 +0200

    (fix) improve volume calculations

--- src/bancor/converters/transform/getVolume.js
+++ src/bancor/converters/transform/getVolume.js
@@ -2,22 +2,45 @@ import BigNumber from 'bignumber.js'
 
 BigNumber.config({ EXPONENTIAL_AT: 18 })
 
+const deriveVolume = ({ tokenSwapTotals, base, from, to }) => {
+  const volumes = tokenSwapTotals.filter(swap => {
+    return swap.fromToken.id === from.address && swap.toToken.id === to.address
+  })
+
+  const totalVolume = volumes.reduce(
+    (result, swap) => {
+      result.totalAmountPurchased = new BigNumber(result.totalAmountPurchased)
+        .plus(swap.totalAmountPurchased || '0')
+        .toString()
+      result.totalAmountReturned = new BigNumber(result.totalAmountReturned)
+        .plus(swap.totalAmountReturned || '0')
+        .toString()
+
+      return result
+    },
+    {
+      totalAmountPurchased: '0',
+      totalAmountReturned: '0'
+    }
+  )
+
+  return totalVolume
+}
+
 export default ({ base, quote, tokenSwapTotals }) => {
-  const buy =
-    tokenSwapTotals.find(swap => {
-      return swap.fromToken.id === base.address && swap.toToken.id === quote.address
-    }) || {}
+  const buyVolume = deriveVolume({ tokenSwapTotals, base, from: base, to: quote })
+  const sellVolume = deriveVolume({ tokenSwapTotals, base, from: quote, to: base })
 
-  const sell =
-    tokenSwapTotals.find(swap => {
-      return swap.fromToken.id === quote.address && swap.toToken.id === base.address
-    }) || {}
+  const rate = new BigNumber(buyVolume.totalAmountPurchased).dividedBy(buyVolume.totalAmountReturned)
+  const validRate = rate.isPositive() && !rate.isZero() && rate.isFinite()
 
-  const volume = new BigNumber(buy.totalAmountPurchased || '0')
-    .plus(buy.totalAmountReturned || '0')
-    .plus(sell.totalAmountPurchased || '0')
-    .plus(sell.totalAmountReturned || '0')
-    .toString()
+  let volume = new BigNumber(0).plus(buyVolume.totalAmountReturned).plus(sellVolume.totalAmountPurchased)
 
-  return volume
+  if (validRate) {
+    volume = volume.plus(
+      new BigNumber(buyVolume.totalAmountPurchased).plus(sellVolume.totalAmountReturned).dividedBy(rate)
+    )
+  }
+
+  return volume.toString()
 }
